ffd26e696b01ac8ce80ba3c6d142d907dfa49dc6
Gihyeonyou <159195897+Gihyeonyou@users.noreply.github.com>
2024-04-10 10:34:36 +1200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+ini_set('display_errors', 'on'); // change to 'off' when live
+
+define ("DB_NAME", "youg2313_Y13_Database");
+define ("DB_USERNAME", "_youg2313");
+define ("DB_PASSWORD", "EMdMz0Js666h5vOV");
+define ("DB_HOST", "localhost");
+
+?>
